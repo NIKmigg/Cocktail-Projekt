a3addf1a247d27172befb637e542acc9e3a461d2
jennmei00 <MeissnerJennifer@dv-schulen.de>
2019-05-09 19:28:33 +0200

    Klassen hinzugefügt

---
+++ CocktailProjekt/Model/Data.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Data
+    {
+        OleDbConnection con;
+
+        public Data()
+        {
+            con = new OleDbConnection(Properties.Settings.Default.ConString);
+        }
+
+        public bool OpenConnection()
+        {
+            Boolean result = true;
+            try
+            {
+                con.Open();
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public DataSet GetAlkohole()
+        {
+            DataSet dsAlkohole = new DataSet();
+            OleDbDataAdapter adapter = new OleDbDataAdapter("Select * from Alkohol", con);
+            adapter.Fill(dsAlkohole, "Alkohole");
+            return dsAlkohole;
+        }
+
+        public DataSet GetSaefte()
+        {
+            DataSet dsSaefte = new DataSet();
+            OleDbDataAdapter adapter = new OleDbDataAdapter("Select * from Saefte", con);
+            adapter.Fill(dsSaefte, "Alkohole");
+            return dsSaefte;
+        }
+
+        public DataSet GetDekos()
+        {
+            DataSet dsDekos = new DataSet();
+            OleDbDataAdapter adapter = new OleDbDataAdapter("Select * from Alkohol", con);
+            adapter.Fill(dsDekos, "Alkohole");
+            return dsDekos;
+        }
+    }
+}
---
+++ CocktailProjekt/Model/Saefte.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Saefte
+    {
+        private Data data = new Data();
+        private ObservableCollection<Saefte> listSaefte;
+
+        public ObservableCollection<Saefte> ListSaefte { get => listSaefte; set => listSaefte = value; }
+
+        public Saefte()
+        {
+            ListSaefte = new ObservableCollection<Saefte>();
+            DataSet ds = data.GetAlkohole();
+            DataTable dt = ds.Tables[0];
+            DataTableReader reader = dt.CreateDataReader();
+            while (reader.Read())
+            {
+                Saft s = mkSaefte(reader);
+                listSaefte.Add(s);
+            }
+            reader.Close();
+        }
+
+        private Saft mkSaefte(DataTableReader reader)
+        {
+            Saft s = new Saft();
+
+            s.Id = reader.GetInt32(0);
+            s.Bezeichnung = reader.GetString(1);
+            s.Preis = reader.GetDecimal(3);
+
+            return s;
+        }
+    }
+}
