a583a8c714300d3fb343098af6827058d82c6ba6
nikmigg <miggenriederniklas@dv-schulen.de>
2019-05-13 15:46:18 +0200

    Alles zusamengefügt

---
+++ CocktailProjekt/ViewModel/UserCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class UserCommand : ICommand
+    {
+        private readonly Action<object> action;  // ist nur im Kostruktor veränderbar
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!parameter.Equals(null)) action(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public UserCommand(Action<object> action)
+        {
+            this.action = action;
+        }
+    }
+}
